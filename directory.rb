4e0fcd2af815f3d0a48dead6e22eeff142c28975
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-03 15:16:02 +0100

    Exercise 1 completed

--- directory.rb
+++ directory.rb
@@ -12,31 +12,14 @@ def input_students
   students
 end
 
-#first we print the list of students
-=begin
-
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Rached", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november}, 
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end 
 def print_header
   puts "The students of Villains Academy"
   puts "---------"
 end 
 
 def print(students)  
-  students.each do |students|
-  puts "#{students[:name]} (#{students[:cohort]} cohort)"
+  students.each_with_index do |students, index|
+    puts "#{index + 1}. #{students[:name]} (#{students[:cohort]} cohort)"
   end
 end 
 
@@ -44,6 +27,7 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students "
 #its important that print() doesnt add new line characters
 end 
+
 
 students = input_students
 print_header
