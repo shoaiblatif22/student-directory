5e22ff22a850d6cad9bf9d2ee27c3b274a344b19
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-03 14:41:46 +0100

    Changed do loop to students instead of names, added in hashes to array (added in name and cohort). Also made corrections to array by adding in commas

--- directory.rb
+++ directory.rb
@@ -1,30 +1,31 @@
 
 #first we print the list of students
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Rached",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Rached", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november}, 
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
   puts "The students of Villains Academy"
   puts "---------"
 end 
 
-def print(names)  
-  names.each do |names|
-  puts names
-end
+def print(students)  
+  students.each do |students|
+  puts "#{students[:name]} (#{students[:cohort]} cohort)"
+  end
+end 
 
-def print_footer(names)
-  print "Overall, we have #{student.count} great students "
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students "
 #its important that print() doesnt add new line characters
 end 
 
